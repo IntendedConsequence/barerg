6d95cc55d5e9f8946b797b621c79d2eeddb90a34
Irwin1138 <irwin1139@gmail.com>
2025-02-17 09:45:03 +0200

    Remove old clang build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set CLANG="c:\program files\llvm\bin\clang-cl.exe"
-set CommonCompilerFlags=/Od
-set CommonCompilerFlags=%CommonCompilerFlags% /Zi
-set CommonLinkerFlags=/incremental:no
-
-%CLANG% %CommonCompilerFlags% barerg.cpp /link %CommonLinkerFlags% kernel32.lib
